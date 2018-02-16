9d9af27774891488329008a808c92cccc81445e7
Ayoolaao <ao.abimbola@gmail.com>
2018-02-16 13:35:45 -0600

    new magicword package to find word in dir

--- src/com/java/io/JavaIO.java
+++ src/com/java/io/JavaIO.java
@@ -1,37 +1,44 @@
 package com.java.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.*;
 
 public class JavaIO {
 
-    static String dir = "./sample.txt";
+    static String path = "./UX_resume_nlb.txt";
+    static String text = "fist";
 
     public static void main(String[] args){
-        //TODO
-
         try {
-//            walkDirectory(dir);
-//            deleteFile(dir);
-            readFile(dir);
+            deleteFile(path);
+            readFile(path);
         } catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    public static void walkDirectory(String path) throws IOException{
-        Files.walk(Paths.get(path)).forEach(f -> System.out.println(f.toFile()));
+    public static boolean searchText(String text, String path){
+        try {
+            return readFile(path).toString().contains(text);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public static void deleteFile(String path) throws IOException{
         Files.delete(Paths.get(path));
     }
 
-    public static void readFile(String path) throws IOException{
+    public static StringBuffer readFile(String path) throws IOException{
         StringBuffer content = new StringBuffer();
 //        Files.lines(Paths.get(path)).forEach(l -> System.out.println(l));
 //        Files.lines(Paths.get(path)).forEach(System.out::println);
+        Files.lines(Paths.get(path)).forEach(l -> content.append(l).append("\n"));
 //        Files.lines(Paths.get(path)).forEach(l -> content.append(l));
-        Files.lines(Paths.get(path)).forEach(content::append);
+//        Files.lines(Paths.get(path)).forEach(content::append);
+        return content;
     }
 }
